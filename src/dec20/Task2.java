bf20a56cedee00cb535892f7f165fa61a3ef4d21
misteraliimran <aaliimran027@gmail.com>
2025-11-05 19:49:33 +0400

    a

---
+++ src/dec20/Task2.java
@@ -0,0 +1,15 @@
+package dec20;
+
+public class Task2 {
+    public static void main(String[] args) {
+        int x = 8;
+        int y = 5;
+
+        System.out.println("x == y: " + (x == y));
+        System.out.println("x != y: " + (x != y));
+        System.out.println("x < y: " + (x < y));
+        System.out.println("x > y: " + (x > y));
+        System.out.println("x <= y: " + (x <= y));
+        System.out.println("x >= y: " + (x >= y));
+    }
+}
---
+++ src/dec20/Task3.java
@@ -0,0 +1,13 @@
+package dec20;
+
+public class Task3 {
+    public static void main(String[] args) {
+        boolean p = true;
+        boolean q = false;
+
+        System.out.println("p AND q: " + (p && q)); 
+        System.out.println("p OR q: " + (p || q));   
+        System.out.println("NOT p: " + (!p));     
+        System.out.println("NOT q: " + (!q));  
+    }
+}
